4d73d95b4b4403f85597879fdea3c9d4f7e320ad
Juswaaa <72263767+Juswaaa@users.noreply.github.com>
2023-08-04 01:00:42 +0800

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,3 +1,10 @@
-# Ask two numbers from the user
+        # Ask two numbers from the user
         first_number = float(input("Please enter the first number: "))
         second_number = float(input("Please enter the second number: "))
+
+        # Print the operation options
+        print("Select operation.")
+        print("1. Add")
+        print("2. Subtract")
+        print("3. Multiply")
+        print("4. Divide")
